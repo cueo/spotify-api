bde283907a9417d16ffe41556c32287ea0d60c76
Mohit Mayank <mohit_mayank@intuit.com>
2017-02-14 15:20:39 +0530

    Sample track file used by create_playlists.py

---
+++ tracks.py
@@ -0,0 +1,161 @@
+'''
+Format:
+Playlist #1
+Song #1
+Song #2
+
+Playlist #2
+Song #1
+...
+'''
+
+tracks = '''Pink Floyd
+Pigs (Three Different Ones)
+If
+Summer '68
+Dark Side of the Moon
+High Hopes
+Lost for Words
+Wot's... Uh The Deal
+See Emily Play
+Let There Be More Light
+Set The Saucerful of Secrets
+Another Brick In The Wall Part 2
+Mother
+Hey You
+Comfortably Numb
+Wish You Were Here
+Welcome To The Machine
+Have A Cigar
+
+The Beatles
+Come Together
+Something
+Here Comes The Sun
+Golden Slumbers
+Carry That Weight
+Ob-La-Di, Ob-La-Da
+While My Guitar Gently Weeps
+Eight Days A Week
+A Hard Day's Night
+Help!
+Yesterday
+Hey Jude
+Let It Be
+Get Back
+The Fool On The Hill
+Blue Jay Way
+Your Mother Should Know
+I Am The Walrus
+Strawberry Fields Forever
+Baby You're A Rich Man
+All You Need Is Love
+P.S. I Love You
+Love Me Do
+Eleanor Rigby
+Norwegian Wood
+Sgt. Pepper's Lonely Hearts Club Band
+With A Little Help From My Friends
+Lucy In The Sky With Diamonds
+A Day In The Life
+Yellow Submarine
+
+Led Zeppelin
+D'yer Mak'er
+Dazed And Confused
+Whole Lotta Love
+Thank You
+Heartbreaker
+Living Loving Maid
+Ramble On
+Immigrant Song
+Gallows Pole
+Black Dog
+Rock And Roll
+Stairway To Heaven
+In My Time Of Dying
+Kashmir
+Achilles Last Stand
+
+Queen
+Bohemian Rhapsody
+Another One Bites The Dust
+Killer Queen
+Don't Stop Me Now
+Somebody To Love
+We Will Rock You
+We Are The Champions
+Radio Gaga
+I Want To Break Free
+Friends Will Be Friends
+The Show Must Go On
+
+The Who
+Baba O'Riley
+Won't Get Fooled Again
+Behind Blue Eyes
+Squeeze Box
+
+The Doors
+Light My Fire
+Roadhouse Blues
+L.A. Woman
+Touch Me
+Hello, I Love You
+Love Her Madly
+Love Me Two Times
+Riders On The Storm
+When The Music's Over
+People Are Strange
+The End
+
+Michael Jackson
+Bad
+The Way You Make Me Feel
+Smooth Criminal
+Thriller
+Beat It
+Billie Jean
+Earth Song
+The Don't Care About Us
+Man In The Mirror
+Will You Be There
+Leave Me Alone
+
+The Velvet Underground
+Sweet Jane
+The Velvet Underground & Nico
+
+U2
+One
+Beautiful Day
+Walk On
+Where The Streets Have No Name
+I Still Haven't Found What I'm Looking For
+With Or Without You
+Sunday Bloody Sunday
+The Hands That Built America
+Hold Me, Thrill Me, Kiss Me, Kill Me
+Vertigo
+
+Metallica
+Enter Sandman
+Sad But True
+The Unforgive
+Wherever I May Roam
+Nothing Else Matters
+Master Of Puppets
+One
+
+Coldplay
+Yellow
+Clocks
+The Scientist
+In My Place
+A Message
+Fix You
+Viva La Vida
+Violet Hill
+Paradise
+Every Teardrop Is A Waterfall
+A Sky Full OF Stars'''
